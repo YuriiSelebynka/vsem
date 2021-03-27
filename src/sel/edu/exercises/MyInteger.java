ee5d93d01a3b49cfe256834345f4468eb4c10456
yurii <selebinkayuriy@gmail.com>
2021-03-28 00:46:33 +0200

    VŠEMvs

--- src/sel/edu/exercises/MyInteger.java
+++ src/sel/edu/exercises/MyInteger.java
@@ -1,22 +1,39 @@
 package sel.edu.exercises;
 
+/**
+ * @author Yurii Selebynka
+ * @project vsem
+ * @class MyInteger
+ * @version 1.0.0
+ * @since 28.03.21 - 00, 38
+ * */
+
+/* Programming patterns. Factory, Builder.
+Develop for your class:
+1. Factory;
+2. Abstract factory;
+3. Create a builder for the class Student;
+4. Create a class MyInteger.
+   Design a method public static boolean isPrime(int num). */
+
+/*4. Create a class MyInteger.
+     Design a method public static boolean isPrime(int num). */
+
 public class MyInteger {
     public static boolean isPrime(int num) {
-        // determine is the number prime or not
+
         for (int i = 2; i < num; i++) {
             if(num % i == 0) {
-                // is not prime number
+                return false;
             }
-
         }
-        return false;
+        return true;
     }
 
-    // 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 - приклади простих чисел Prime numbers
     public static void main(String[] args) {
         System.out.println(isPrime(11)); // true
         System.out.println(isPrime(21)); // false
         System.out.println(isPrime(31)); // true
-        System.out.println(isPrime(1001)); // ??-
+        System.out.println(isPrime(1001)); // false
     }
 }
